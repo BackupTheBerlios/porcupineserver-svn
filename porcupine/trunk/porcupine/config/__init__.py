a8a57cf85e09e11b2cd0009d4a64f64b3d2cd124
tkouts <tkouts@4958b8bd-3100-0410-97a7-c2b44273271c>
2006-02-26 00:39:14 +0000

    >>>>Updated doc strings in porcupine.config pakage.

--- porcupine/trunk/porcupine/config/__init__.py
+++ porcupine/trunk/porcupine/config/__init__.py
@@ -14,4 +14,4 @@
 #    along with Porcupine; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #===============================================================================
-
+"Server configuration related classes"
