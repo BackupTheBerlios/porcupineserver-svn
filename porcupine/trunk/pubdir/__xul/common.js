a89b850120239f44d5881c983b7c94efc5f7f20a
tkouts <tkouts@4958b8bd-3100-0410-97a7-c2b44273271c>
2006-09-04 01:38:31 +0000

    QuiX GroupBox:   - added support for checked GroupBox.

--- porcupine/trunk/pubdir/__xul/common.js
+++ porcupine/trunk/pubdir/__xul/common.js
@@ -38,8 +38,7 @@ IFrame.prototype.getSource = function() {
 }
 
 // GroupBox
-function GroupBox(params)
-{
+function GroupBox(params) {
 	params = params || {};
 	params.overflow = 'hidden';
 	this.base = Widget;
@@ -49,31 +48,76 @@ function GroupBox(params)
 	this.border = new Widget({
 		top: 8,
 		width:"100%",
-		padding:"8,8,8,8",
+		padding:"12,12,12,12",
 		height: "this.parent.getHeight()-this.getTop()",
 		border: params.border || 2
 	});
 	this.border.div.className = "groupboxframe";
 	this.appendChild(this.border);
 
-	this.caption = new Label({
-		left:5,
-		bgcolor: params.bgcolor,
-		caption: params.caption
-	});
+	var checked = true;
+	if (params.checked) {
+		checked = params.checked=='true' || params.checked==true;
+		this.caption = new Field({
+			left: 5,
+			bgcolor: params.bgcolor,
+			caption: params.caption,
+			border: "thin",
+			value: checked,
+			onclick: GroupBox__checkBody,
+			type: "checkbox"
+		});
+	}
+	else
+		this.caption = new Label({
+			left:5,
+			bgcolor: params.bgcolor,
+			caption: params.caption
+		});
+
 	this.appendChild(this.caption);
 	this.caption.div.className = this.div.className;
 
 	this.body = new Widget({
 		width: "100%",
-		height: "100%"
+		height: "100%",
+		disabled: !checked
 	});
 	this.border.appendChild(this.body);
 }
 
 GroupBox.prototype = new Widget;
 
+GroupBox.prototype.customEvents = Widget.prototype.customEvents.concat(['onactivate', 'ondeactivate']);
+
 GroupBox.prototype.setBgColor = function(color) {
 	Widget.prototype.setBgColor(color,this);
 	this.caption.setBgColor(color);
+}
+
+GroupBox.prototype.getValue = function() {
+	return (this.caption.getValue)?this.caption.getValue():true;
+}
+
+GroupBox.prototype.setValue = function(value) {
+	if (this.caption.setValue) {
+		this.caption.setValue(value);
+		GroupBox__checkBody(null, this.caption);
+	}
+}
+
+function GroupBox__checkBody(evt ,w) {
+	var box = w.parent;
+	if (w.getValue()) {
+		box.body.enable();
+		if (box._customRegistry.onactivate)
+			box._customRegistry.onactivate(evt, box);
+	}
+	else {
+		box.body.disable();
+		if (box._customRegistry.ondeactivate)
+			box._customRegistry.ondeactivate(evt, box);
+	}
+	if (evt)
+		QuiX.stopPropag(evt);
 }
