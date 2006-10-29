9076e738ecefaf07a919e5e5ec48510f245fa32a
tkouts <tkouts@4958b8bd-3100-0410-97a7-c2b44273271c>
2006-10-29 18:54:49 +0000

    >>>> Tagging Porcupine 0.0.8

---
+++ porcupine/tags/0.0.8/org/innoscript/desktop/generic.js
@@ -0,0 +1,160 @@
+function generic() {}
+
+generic.showObjectProperties = function(evt, w, o, refresh_func) {
+	document.desktop.parseFromUrl(QuiX.root	+ (o.id || o) + '?cmd=properties',
+		function(w) {
+			if (refresh_func) w.attributes.refreshlist = refresh_func;
+		}
+	);
+}
+
+generic.selectItems = function(evt, w) {
+	var oDialog = w.getParentByType(Dialog);
+	var oTarget = w.parent.parent.getWidgetsByType(SelectList)[0];
+	var sFolderURI = oTarget.attributes.SelectFrom;
+	var sCC = oTarget.attributes.RelatedCC;
+	generic.selectObjects(oDialog, oTarget, generic.addSelectionToList, sFolderURI, sCC, 'true');
+}
+
+generic.selectItem = function(evt, w) {
+	var oDialog = w.getParentByType(Dialog);
+	var oTarget = w.parent;
+	var sFolderURI = oTarget.attributes.SelectFrom;
+	var sCC = oTarget.attributes.RelatedCC;
+	generic.selectObjects(oDialog, oTarget, generic.addSelectionToField, sFolderURI, sCC, 'false');
+}
+
+generic.selectObjects = function(win, target, select_func, startFrom, contentclass, multiple) {
+	win.showWindow(startFrom + '?cmd=selectobjects&cc=' + contentclass + '&multiple=' + multiple,
+		function(win) {
+			win.attributes['target'] = target;
+			win.attributes['select_click'] = select_func;
+		}
+	);
+}
+
+generic.addSelectionToList = function(evt, w, source, target) {
+	for (var i=0; i<source.options.length; i++) {
+		var oOption = source.options[i];
+		if (oOption.selected) {
+			target.addOption(
+				{
+					img : oOption.img, 
+					caption : oOption.getCaption(), 
+					value : oOption.value
+				}
+			);
+		}
+	}
+}
+
+generic.addSelectionToField = function(evt, w, source, target) {
+	for (var i=0; i<source.options.length; i++) {
+		var oOption = source.options[i];
+		if (oOption.selected) {
+			target.getWidgetsByType(Field)[0].setValue(oOption.value);
+			target.getWidgetsByType(Field)[1].setValue(oOption.getCaption());
+			return;
+		}
+	}
+}
+
+generic.removeSelectedItems = function(evt, w) {
+	var oSelectList = w.parent.parent.getWidgetsByType(SelectList)[0];
+	oSelectList.removeSelected();
+}
+
+generic.clearReference1 = function(evt, w) {
+	var fields = w.parent.getWidgetsByType(Field);
+	fields[0].setValue('');
+	fields[1].setValue('');
+}
+
+generic.updateItem = function(evt, w) {
+	var oDialog = w.getParentByType(Dialog);
+	var oForm = oDialog.getWidgetsByType(Form)[0];
+	oForm.submit(
+		function() {
+			if (oDialog.attributes.refreshlist) oDialog.attributes.refreshlist();
+			oDialog.close();
+		}
+	);
+}
+
+generic.openContainer = function(evt, w) {
+	document.desktop.parseFromUrl(QuiX.root	+ w.attributes.folderID + '?cmd=list');
+}
+
+generic.runApp = function(evt,w) {
+	var appUrl = w.attributes.url;
+	document.desktop.parseFromUrl(QuiX.root	+ appUrl);
+}
+
+generic.addSelectionToAclDataGrid = function(evt, w, source, target) {
+	for (var i=0; i<source.options.length; i++) {
+		var oOption = source.options[i];
+		if (oOption.selected) {
+			target.dataSet.push(
+				{
+					id : oOption.value, 
+					displayName : oOption.getCaption(), 
+					role: '1'
+				}
+			);
+		}
+	}
+	target.refresh();
+}
+
+generic.rolesInherited_onclick = function(evt, w) {
+	var _acl = w.parent.widgets[1];
+	if (!w.getValue()) _acl.enable();
+	else _acl.disable();
+}
+
+generic.getSecurity = function(tabcontrol, itab) {
+	var acl_datagrid = tabcontrol.tabs[itab].getWidgetsByType(DataGrid)[0];
+	var sObjectURI = tabcontrol.getParentByType(Form).action;
+	var xmlrpc = new XMLRPCRequest(sObjectURI);
+	xmlrpc.oncomplete = function(req) {
+		acl_datagrid.dataSet = req.response;
+		acl_datagrid.refresh();
+	};
+	xmlrpc.callmethod('getSecurity');
+}
+
+generic.addACLEntry = function(evt, w) {
+	var oDialog = w.getParentByType(Dialog);
+	var oDataGrid = w.parent.parent.getWidgetsByType(DataGrid)[0];
+	var sFolderURI = QuiX.root + 'users';
+	generic.selectObjects(oDialog, oDataGrid, generic.addSelectionToAclDataGrid, sFolderURI, '*');
+}
+
+generic.removeACLEntry = function(evt, w) {
+	var oDataGrid = w.parent.parent.getWidgetsByType(DataGrid)[0];
+	oDataGrid.removeSelected();
+}
+
+generic.computeSize = function(obj, value) {
+	if (value)
+		return Math.round(value/1024) + ' KB';
+	else
+		return '';
+}
+
+generic.getProcessDialog = function(title, steps, oncomplete) {
+	var dlg = document.desktop.parseFromString('<a:dialog xmlns:a="http://www.innoscript.org/quix"'+
+		' title="' + title + '" width="240" height="100" left="center" top="center">' +
+		'<a:prop type="bool" name="canceled" value="0"></a:prop>' +
+		'<a:wbody>' +
+		'<a:progressbar id="pb" width="90%" height="24" left="center" top="center" ' +
+		'maxvalue="' + steps + '">' +
+		'<a:label align="center" width="100%" height="100%" caption="0%">' +
+		'</a:label></a:progressbar></a:wbody>' +
+		'<a:dlgbutton onclick="generic.cancelAction" width="70" height="22" caption="' + document.desktop.attributes.CANCEL + '"></a:dlgbutton>' +
+		'</a:dialog>', oncomplete);
+}
+
+generic.cancelAction = function(evt, w) {
+	w.getParentByType(Dialog).attributes.canceled = true;
+}
---
+++ porcupine/tags/0.0.8/quix/box.js
@@ -0,0 +1,128 @@
+/************************
+Box layout
+************************/
+function Box(params) {
+	params = params || {};
+	params.overflow = params.overflow || 'hidden';
+	this.base = Widget;
+	this.base(params);
+	this.div.className = 'box';
+	this.orientation = params.orientation || 'h';
+	var iSpacing = params.spacing || 2;
+	this.spacing = parseInt(iSpacing);
+	this.childrenAlign = params.childrenalign;
+}
+
+Box.prototype = new Widget;
+
+Box.prototype.appendChild = function(w) {
+	w.destroy = BoxWidget__destroy;
+	if (this.orientation == 'h')
+		w.height = w.height || '100%';
+	else
+		w.width = w.width || '100%';
+	Widget.prototype.appendChild(w, this, true);
+	this.redraw(true);
+}
+
+Box.prototype.redraw = function(bForceAll) {
+	var oWidget, boxalign;
+	if (bForceAll) {
+		var offset_var = (this.orientation=='h')?'left':'top';
+		var center_var = (this.orientation=='h')?'top':'left';
+		var length_var = (this.orientation=='h')?'width':'height';
+		
+		for (var i=0; i<this.widgets.length; i++) {
+			oWidget = this.widgets[i];
+			oWidget[offset_var] = 'this.parent._getWidgetOffset(' + i + ')';
+			boxalign = oWidget.boxAlign || this.childrenAlign;
+			if (
+				(this.orientation=='h' && (boxalign == 'center' || boxalign=='bottom')) ||
+				(this.orientation=='v' && (boxalign == 'center' || boxalign=='right')) )
+			{
+				oWidget[center_var] = 'this.parent._getWidgetPos(' + i + ')';
+			}
+	
+			if (oWidget[length_var] == null || oWidget[length_var] == '-1')
+				oWidget[length_var] = 'this.parent._calcWidgetLength()';
+		}
+	}
+	Widget.prototype.redraw(bForceAll, this);
+}
+
+Box.prototype._getWidgetPos = function(iPane)
+{
+	var oWidget = this.widgets[iPane];
+	var boxalign =  oWidget.boxAlign || this.childrenAlign;
+	var w1 = (this.orientation=='h')?this.parent.getHeight():this.parent.getWidth();
+	var w2 = (this.orientation=='h')?oWidget.getHeight(true):oWidget.getWidth(true);
+	switch (boxalign)
+	{
+		case 'center':
+				return (w1 - w2)/2;
+		case 'right':
+		case 'bottom':
+				return (w1 - w2);
+		default: 	
+				return 0;
+	}
+}
+
+
+Box.prototype._getWidgetOffset=function(iPane) {
+	var offset = 0;
+	if (iPane > 0)
+	{
+		var i = iPane - 1;
+		var ow = this.widgets[i];
+		while (ow.isHidden() && i >= 0) {
+			ow = this.widgets[i];
+			i -= 1;
+		}
+		if (this.orientation=='h')
+			offset = ow.getLeft() + ow.getWidth(true) + this.spacing;
+		else
+			offset = ow.getTop() + ow.getHeight(true) + this.spacing;
+	}
+	return(offset);
+}
+
+Box.prototype._calcWidgetLength = function() {
+	var tl = 0;
+	var free_widgets = 0;
+	var length_var = (this.orientation=='h')?'width':'height';
+	
+	for (var i=0; i<this.widgets.length; i++) {
+		if (this.widgets[i].isHidden()) continue;
+		if (this.widgets[i][length_var] != 'this.parent._calcWidgetLength()') {
+			if (this.orientation=='h')
+				tl += this.widgets[i]._calcWidth(true);
+			else
+				tl += this.widgets[i]._calcHeight(true);
+		}
+		else
+			free_widgets += 1;
+	}
+	var l = (this.orientation=='h')?this.getWidth():this.getHeight();
+	
+	var nl = (l - tl - ((this.widgets.length-1)*this.spacing)) / free_widgets;
+	return(nl>0?nl:0);
+}
+
+function BoxWidget__destroy() {
+	var oBox = this.parent;
+	var length_var = (oBox.orientation=='h')?'width':'height';
+	
+	for (var idx=0; idx < oBox.widgets.length; idx++) {
+		 if (oBox.widgets[idx] == this)
+		 	break;
+	}
+	if (this[length_var] == '-1' && oBox.widgets.length > 1) {
+		if (idx == 0)
+			oBox.widgets[1][length_var] = '-1';
+		else
+			oBox.widgets[idx-1][length_var] = '-1';
+	}
+	Widget.prototype.destroy(this);
+	oBox.redraw(true);
+}
