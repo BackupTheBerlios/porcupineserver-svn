403eac531ecaa345b77005278c6ff43552d0e9a8
tkouts <tkouts@4958b8bd-3100-0410-97a7-c2b44273271c>
2007-02-12 21:14:13 +0000

    made a copy

---
+++ porcupine/tags/0.0.9/porcupine/config/registrations.py
@@ -0,0 +1,148 @@
+#===============================================================================
+#    Copyright 2005, 2006 Tassos Koutsovassilis
+#
+#    This file is part of Porcupine.
+#    Porcupine is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation; either version 2.1 of the License, or
+#    (at your option) any later version.
+#    Porcupine is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with Porcupine; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#===============================================================================
+"Server registrations"
+
+import re
+from xml.dom import minidom
+from xml.parsers import expat
+
+from porcupine import serverExceptions
+from porcupine.utils import misc
+
+def getFiltersList(contextNode):
+    filterList = contextNode.getElementsByTagName('filter')
+    filters = []
+    for filterNode in filterList:
+        type = filterNode.getAttribute('type').encode('iso-8859-1')
+        filter = [misc.getClassByName(type), {}]
+        for attr in filterNode.attributes.keys():
+            filter[1][str(attr)] = filterNode.getAttribute(attr).encode('iso-8859-1')
+        filters.append( tuple(filter) )
+    return tuple(filters)
+
+class Registration(object):
+    __slots__ = ('context', 'type', 'encoding', 'filters', 'max_age')
+    def __init__(self, identifier, enc, filters, max_age):
+        try:
+            self.context = misc.getClassByName(identifier)
+            self.type = 2
+        except:
+            self.context = identifier
+            if identifier[-4:] == '.psp':
+                self.type = 1
+            else:
+                self.type = 0
+        
+        self.encoding = enc
+        self.filters = filters
+        self.max_age = int(max_age);
+
+class App(object):
+    def __init__(self, appNode):
+        appName = appNode.getAttribute('name')
+        self.path = appNode.getAttribute('path')
+        self.__config = []
+        self.__cache = {}
+        configXML = minidom.parse(self.path + '/config.xml')
+        contextList = configXML.getElementsByTagName('context')
+        # construct action list
+        for contextNode in contextList:
+            sPath = contextNode.getAttribute('path')
+            sMethod = contextNode.getAttribute('method')
+            sBrowser = contextNode.getAttribute('client')
+            sLang = contextNode.getAttribute('lang')
+            sAction = contextNode.getAttribute('action')
+            encoding = contextNode.getAttribute('encoding').encode('iso-8859-1') or None
+            max_age = contextNode.getAttribute('max-age') or 0
+            
+            self.__config.append((
+                (sPath, sMethod, sBrowser, sLang),
+                Registration(self.path + '/' + sAction, encoding,
+                             getFiltersList(contextNode), max_age)
+            ))
+            
+        configXML.unlink()
+
+    def getRegistration(self, sPath, sHttpMethod, sBrowser, sLang):
+        if self.__cache.has_key((sPath, sHttpMethod, sBrowser, sLang)):
+            return self.__cache[(sPath, sHttpMethod, sBrowser, sLang)]
+        else:
+            for paramList in self.__config:
+                Path, HttpMethod, Browser, Lang = paramList[0]
+                if Path==sPath and re.search(HttpMethod, sHttpMethod) and re.search(Browser, sBrowser) and re.search(Lang, sLang):
+                    registration = paramList[1]
+                    self.__cache[(sPath, sHttpMethod, sBrowser, sLang)] = registration
+                    return registration
+            self.__cache[(sPath, sHttpMethod, sBrowser, sLang)] = None
+            return None
+
+class StoreConfiguration(object):
+    def __init__(self):
+        self.__config = []
+        self.__cache = {}
+        try:
+            configXML = minidom.parse('conf/store.xml')
+        except expat.ExpatError, v:
+            raise serverExceptions.ConfigurationError, 'Error parsing store.xml (%s)' % v[0]
+        regList = configXML.getElementsByTagName('reg')
+        # construct action list
+        for regNode in regList:
+            sCC = regNode.getAttribute('cc')
+            sMethod = regNode.getAttribute('method')
+            sParam = regNode.getAttribute('param')
+            sBrowser = regNode.getAttribute('client')
+            sLang = regNode.getAttribute('lang')
+            sAction = regNode.getAttribute('action')
+            encoding = regNode.getAttribute('encoding').encode('iso-8859-1') or None
+            max_age = regNode.getAttribute('max-age') or 0
+            
+            self.__config.append((
+                 (sCC, sMethod, sParam, sBrowser, sLang),
+                 Registration(sAction, encoding, getFiltersList(regNode), max_age)
+            ))
+            
+        configXML.unlink()
+
+    def getRegistration(self, sCC, sHttpMethod, sParam, sBrowser, sLang):
+        if self.__cache.has_key((sCC, sHttpMethod, sParam, sBrowser, sLang)):
+            return self.__cache[(sCC, sHttpMethod, sParam, sBrowser, sLang)]
+        else:
+            for paramList in self.__config:
+                CC, HttpMethod, Param, Browser, Lang = paramList[0]
+                #print CC, HttpMethod, Param, Browser, Lang
+                if re.search(CC, sCC) and re.search(HttpMethod, sHttpMethod) and Param == sParam and re.search(Browser, sBrowser) and re.search(Lang, sLang):
+                    registration = paramList[1]
+                    self.__cache[(sCC, sHttpMethod, sParam, sBrowser, sLang)] = registration
+                    return registration
+            self.__cache[(sCC, sHttpMethod, sParam, sBrowser, sLang)] = None
+            return None
+
+apps = {}
+
+try:
+    configDom = minidom.parse('conf/pubdir.xml')
+except expat.ExpatError, v:
+    raise serverExceptions.ConfigurationError, 'Error parsing apps.xml (%s)' % v[0]
+
+for appNode in configDom.getElementsByTagName('dir'):
+    webApp = App(appNode)
+    apps[appNode.getAttribute('name')] = webApp
+
+configDom.unlink()
+del configDom
+
+storeConfig = StoreConfiguration()
---
+++ porcupine/tags/0.0.9/quix/formfields.js
@@ -0,0 +1,368 @@
+/************************
+Form & Field controls
+************************/
+function Form(params) {
+	params = params || {};
+	params.width = params.width || '100%';
+	params.height = params.height || '100%';
+	this.base = Widget;
+	this.base(params);
+	this.files = [];
+	this.action = params.action;
+	this.method = params.method;
+	this.elements = [];
+}
+
+Form.prototype = new Widget;
+
+Form.prototype.getElementByName = function(name) {
+	for (var i=0; i<this.elements.length; i++) {
+		if (this.elements[i].name==name) return(this.elements[i]);
+	}
+}
+
+Form.prototype.submit = function(f_callback) {
+	function submit_oncomplete(req) {
+		var form = req.callback_info;
+		f_callback(req.response, form);
+	}
+	
+	var formData = {};
+	// build form data
+	for (var i=0; i<this.elements.length; i++) {
+		if (this.elements[i].name && !this.elements[i]._isDisabled)
+			formData[this.elements[i].name] = this.elements[i].getValue();
+	}
+	// send data
+	var xmlrpc = new XMLRPCRequest(this.action);
+	xmlrpc.oncomplete = submit_oncomplete;
+	xmlrpc.callback_info = this;
+	xmlrpc.callmethod(this.method, formData);
+}
+
+function Field(params) {
+	params = params || {};
+	this.base = Widget;
+	params.border = params.border || 1;
+	params.overflow = 'hidden';
+	this.type = params.type || 'text';
+	if (this.type == 'radio') {
+		this._value = params.value || '';
+		params.onclick = QuiX.getEventWrapper(Radio__click, params.onclick);
+		params.overflow = '';
+	}
+	params.height = params.height || 22;
+	params.padding = '0,0,0,0';
+	this.base(params);
+	this.name = params.name;
+	this.readonly = (params.readonly=='true' || params.readonly==true)?true:false;
+	this.onchange = params.onchange;
+	var e;
+	switch (this.type) {
+		case 'checkbox':
+		case 'radio':
+			var sChecked;
+			if (this.type=='checkbox')
+				sChecked = (params.value==true || params.value == 'true')?'checked':'';
+			else
+				sChecked = (params.checked==true || params.checked == 'true')?'checked':'';
+			this.div.innerHTML = '<input type=' + this.type + ' ' + sChecked +
+				' style="vertical-align:middle">';
+			e = this.div.firstChild;
+			if (this.readonly) e.disabled = true;
+			if (params.caption) this.setCaption(params.caption);
+			break;
+		case 'file':
+			throw new QuiX.Exception("Invalid field type.\nUse the file control instead.")
+			break;
+		default:
+			this.div.className = 'field';
+			e = (this.type=='textarea')?ce('TEXTAREA'):ce('INPUT');
+			e.style.borderWidth = '1px';
+			e.style.position='absolute';
+			if (this.readonly) e.readOnly = true;
+			//fixme: wait for MS to fix it
+			//e.style.width = '100%';
+			//e.style.height = '100%';
+			if (this.type!='textarea') e.type = this.type;
+			e.value = (params.value)?params.value:'';
+			this.textPadding = params.textPadding || 0;
+			if (this.type=='hidden') this.hide();
+			this.div.appendChild(e);
+			//this.setPadding( params.padding.split(',') );
+	}
+
+	e.onmousedown = QuiX.stopPropag;
+	e.onselectstart = QuiX.stopPropag;
+	
+	var oField = this;
+	e.onchange = function() {
+		if (oField.onchange) getEventListener(oField.onchange)(oField);
+	}
+
+	this._adjustFieldSize();
+	if (this._isDisabled) {
+		e.disabled = true;
+		e.style.backgroundColor = 'menu';
+	}
+}
+
+Field.prototype = new Widget;
+
+Field.prototype.getValue = function() {
+	switch (this.type) {
+	case 'checkbox':
+		return this.div.firstChild.checked;
+	case 'radio':
+		var radio;
+		var id = this.getId();
+		if (id) {
+			var radio_group = this.parent.getWidgetById(id);
+			for (var i=0; i<radio_group.length; i++) {
+				radio = radio_group[i].div.firstChild;
+				if (radio.checked)
+					return (radio_group[i]._value);
+			}
+		}
+		break;
+	default:
+		return this.div.firstChild.value;
+	}
+}
+
+Field.prototype.setValue = function(value) {
+	switch (this.type) {
+	case 'checkbox':
+		this.div.firstChild.checked = value;
+		break;
+	case 'radio':
+		var radio;
+		var id = this.getId();
+		if (id) {
+			var radio_group = this.parent.getWidgetById(id);
+			for (var i=0; i<radio_group.length; i++) {
+				radio = radio_group[i].div.firstChild;
+				if (radio_group[i]._value == value)
+					radio.checked = true;
+				else
+					radio.checked = false;
+			}
+		}
+		break;
+	default:
+		this.div.firstChild.value = value;
+	}
+}
+
+Field.prototype.getCaption = function() {
+	if (this.type=='radio' || this.type=='checkbox') {
+		var oSpan = this.div.getElementsByTagName('SPAN')[0];
+		if (oSpan)
+			return oSpan.innerHTML;
+		else
+			return '';
+	}
+}
+
+Field.prototype.setCaption = function(caption) {
+	if (this.type=='radio' || this.type=='checkbox') {
+		var textnode = this.div.getElementsByTagName('SPAN')[0];
+		if (!textnode) {
+			textnode =ce('SPAN');
+			textnode.style.cursor = 'default';
+			textnode.innerHTML = caption;
+			textnode.style.verticalAlign = 'middle';
+			this.div.appendChild(textnode);
+		}
+		else {
+			textnode.innerHTML = caption;
+		}
+	}
+}
+
+Field.prototype.enable = function() {
+	if (this.div.firstChild) {
+		this.div.firstChild.disabled = false;
+		this.div.firstChild.style.backgroundColor = this.getBgColor();
+	}
+	Widget.prototype.enable(this);
+}
+
+Field.prototype.disable = function() {
+	if (this.div.firstChild) {
+		this.div.firstChild.disabled = true;
+		this.div.firstChild.style.backgroundColor = 'menu';
+	}
+	Widget.prototype.disable(this);
+}
+
+Field.prototype.setBgColor = function(color) {
+	this.div.style.backgroundColor = color;
+	if (this.type == 'text' || this.type == 'textarea' || this.type == 'password')
+		this.div.firstChild.style.backgroundColor = color;
+}
+
+Field.prototype.redraw = function(bForceAll) {
+	if (this.type == 'text' || this.type == 'textarea' || this.type == 'password')
+		this.div.firstChild.style.padding = this.textPadding + 'px' + ' ' + this.textPadding + 'px'+ ' ' + this.textPadding + 'px'+ ' ' + this.textPadding + 'px';
+	Widget.prototype.redraw(bForceAll, this);
+}
+
+Field.prototype._adjustFieldSize = function() {
+	if (this.type!='checkbox' && this.type!='radio' && this.div.firstChild) {
+		var nw = this.getWidth() || 0;
+		var nh = this.getHeight() || 0;
+		nw -= 1;
+		nh -= 1;
+		if (this.type=='textarea' && QuiX.browser=='moz')
+			this.div.firstChild.style.top = '-1px';
+		if (nw < 0) nw = 0;
+		if (nh < 0) nh = 0;
+		this.div.firstChild.style.width = nw + 'px';
+		this.div.firstChild.style.height = nh + 'px';
+	}
+}
+
+Field.prototype._setCommonProps = function() {
+	this.base.prototype._setCommonProps(this);
+	this._adjustFieldSize();
+}
+
+function Radio__click(evt, w) {
+	var id = w.getId();
+	if (id) {
+		var radio_group = w.parent.getWidgetById(id);
+		for (var i=0; i<radio_group.length; i++) {
+			radio = radio_group[i].div.firstChild;
+			radio.checked = false;
+		}
+		w.div.firstChild.checked = true;
+	}
+}
+
+// spin button
+function Spin(params) {
+	params = params || {};
+	params.bgcolor = params.bgcolor || 'white';
+	params.border = params.border || 1;
+	params.padding = '0,0,0,0';
+	params.overflow = 'hidden';
+	params.height = params.height || 22;
+
+	this.base = Widget;
+	this.base(params);
+
+	this.div.className = 'combo';
+		
+	this.name = params.name;
+	this.editable = (params.editable=='true' || params.editable==true)?true:false;
+	this.min = params.min || 0;
+	this.max = params.max;
+	this.div.className = 'field';
+	this.onchange = params.onchange;
+
+	var e = ce('INPUT');
+	e.style.borderWidth = '1px';
+	e.style.position='absolute';
+	e.style.textAlign = 'right';
+	this.div.appendChild(e);
+	e.onmousedown = QuiX.stopPropag;
+	e.onselectstart = QuiX.stopPropag;
+	
+	if (params.maxlength)
+		e.maxLength = params.maxlength;
+	
+	var oSpin = this;
+
+	upbutton = new XButton({
+		left : "this.parent.getWidth()-16",
+		height : '50%', width : 16,
+		onclick : SpinUp__onclick
+	});
+	this.appendChild(upbutton);
+
+	downbutton = new XButton({
+		left : "this.parent.getWidth()-16",
+		height : '50%', top : '50%', width : 16,
+		onclick : SpinDown__onclick
+	});
+	this.appendChild(downbutton);
+
+	if (!this.editable) {
+		e.readOnly = true;
+		e.style.cursor = 'default';
+	} else {
+		e.onblur = function() {oSpin.validate();}
+	}
+	
+	this.attachEvent('onkeypress', Spin__onkeypress);
+	
+	if (params.value)
+		this.setValue(params.value);
+}
+
+Spin.prototype = new Widget;
+
+Spin.prototype._adjustFieldSize = function() {
+	if (this.div.firstChild) {
+		var nh = this.getHeight();
+		var offset = (QuiX.browser=='ie')?20:18;
+		this.div.firstChild.style.width = (this.getWidth()-offset) + 'px';
+		this.div.firstChild.style.height = nh + 'px';
+	}
+}
+
+Spin.prototype._setCommonProps = function() {
+	Widget.prototype._setCommonProps(this);
+	this._adjustFieldSize();
+}
+
+Spin.prototype.validate = function() {
+	var min = this.min;
+	var max = this.max;
+	var val = this.getValue();
+	if (max && val > max ) this.setValue(max);
+	if (val < min) this.setValue(min);
+}
+
+Spin.prototype.setBgColor = function(color) {
+	this.div.style.backgroundColor = color;
+	if (this.div.firstChild)
+		this.div.firstChild.style.backgroundColor = color;
+}
+
+Spin.prototype.getValue = function() {
+	return( parseInt(this.div.firstChild.value) );
+}
+
+Spin.prototype.setValue = function(value) {
+	if (value != this.getValue()) {
+		this.div.firstChild.value = parseInt(value);
+		if (this.onchange)
+			getEventListener(this.onchange)(this);
+	}
+}
+
+function Spin__onkeypress(evt, w) {
+	var keycode = (QuiX.browser=='ie')? evt.keyCode:evt.charCode;
+	if (!(keycode>47 && keycode<58) && keycode!=0)
+		QuiX.cancelDefault(evt);
+}
+
+function SpinUp__onclick(evt, w) {
+	var oSpin = w.parent;
+	var val = oSpin.getValue() + 1;
+	if (!isNaN(val)) {
+		oSpin.setValue(val);
+		oSpin.validate();
+	}
+}
+
+function SpinDown__onclick(evt, w) {
+	var oSpin = w.parent;
+	var val = oSpin.getValue() - 1;
+	if ( !isNaN(val)) {
+		oSpin.setValue(val);
+		oSpin.validate();
+	}
+}
