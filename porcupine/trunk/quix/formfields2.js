03de29d5630c2ce82a3943d32f6b5c2c076205fa
tkouts <tkouts@4958b8bd-3100-0410-97a7-c2b44273271c>
2008-02-12 00:48:45 +0000

    QuiX SelectList:   - removed previous optimizations for SelectList options. The are unneeded.

--- porcupine/trunk/quix/formfields2.js
+++ porcupine/trunk/quix/formfields2.js
@@ -393,9 +393,4 @@ function SelectOption__onmousedown(evt, option) {
 			else
 				oSelectList.selectOption(option);
 	}
-	if (QuiX.getMouseButton(evt) == 0) {
-		QuiX.cleanupOverlays();
-		QuiX.stopPropag(evt);
-		QuiX.cancelDefault(evt);
-	}
 }
