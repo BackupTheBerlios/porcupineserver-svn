c2bdb48803f0778037dd81a1740d6b874887b33c
tkouts <tkouts@4958b8bd-3100-0410-97a7-c2b44273271c>
2008-02-26 17:25:30 +0000

    QuiX core:   - fixed file control dependencies   - fixed mac scroll bars bleed through for the progress widget

--- porcupine/trunk/quix/compat.js
+++ porcupine/trunk/quix/compat.js
@@ -44,10 +44,11 @@ QuiX.constructors = {
 QuiX.progress = '<rect id="quix_progress" xmlns="http://www.innoscript.org/quix" ' +
 	'width="320" height="56" overflow="auto" top="center" left="center" ' +
 	'border="2" bgcolor="white" style="border-color:#999999;border-style:solid" '+
-	'padding="4,4,4,4"><xhtml><![CDATA[<center>Please wait...<br/><br/>' +
+	'padding="4,4,4,4"><rect width="100%" height="100%" overflow="hidden">' +
+	'<xhtml><![CDATA[<center>Please wait...<br/><br/>' +
 	'<span></span></center>]]></xhtml>' +
 	'<progressbar id="pb" width="150" maxvalue="1" height="4" ' +
-	'top="center" left="center"></progressbar></rect>';
+	'top="center" left="center"></progressbar></rect></rect>';
 
 QuiX.modules = [
 	new QModule('Windows and Dialogs', '__quix/windows.js', [3,15]),
@@ -61,7 +62,7 @@ QuiX.modules = [
 	new QModule('Forms & Fields', '__quix/formfields.js', [3]),
 	new QModule('Common Widgets', '__quix/common.js', [3]),
 	new QModule('Datagrid', '__quix/datagrid.js', [5,8]),
-	new QModule('File Control', '__quix/file.js', [1,3,8]),
+	new QModule('File Control', '__quix/file.js', [1,3,8,14]),
 	new QModule('Date Picker', '__quix/datepicker.js', [14]),
 	new QModule('Timers', '__quix/timers.js', []),
 	new QModule('Forms & Fields 2', '__quix/formfields2.js', [3]),
