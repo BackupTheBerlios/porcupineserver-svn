e973afe7a2138f1a8fe1b51d5e2cfe5ef5ec92a8
tkouts <tkouts@4958b8bd-3100-0410-97a7-c2b44273271c>
2008-07-11 14:43:22 +0000

    QuiX:   - new AutoComplete widget

--- porcupine/trunk/quix/formfields2.js
+++ porcupine/trunk/quix/formfields2.js
@@ -1,6 +1,7 @@
 /************************
 Field controls 2
 ************************/
+
 // combo box
 function Combo(params) {
 	params = params || {};
@@ -8,12 +9,10 @@ function Combo(params) {
 	params.border = params.border || 1;
 	params.overflow = 'hidden';
 	params.height = params.height || 22;
-
+	
 	this.base = Widget;
 	this.base(params);
-
-	this.div.className = 'combo';
-		
+	
 	this.name = params.name;
 	this.editable = (params.editable=='true' || params.editable==true)?true:false;
 	this.readonly = (params.readonly=='true' || params.readonly==true)?true:false;
@@ -22,7 +21,7 @@ function Combo(params) {
 	this.selection = null;
 	this.isExpanded = false;
 	this.attachEvent('onmousedown', QuiX.stopPropag);
-
+	
 	var e = ce('INPUT');
 	e.style.borderWidth = '1px';
 	e.style.position = 'absolute';
@@ -30,7 +29,7 @@ function Combo(params) {
 	e.onselectstart = QuiX.stopPropag;
 	
 	var oCombo = this;
-
+	
 	this.dropdown = new Widget({
 		border : 1,
 		onclick : function(evt, w) {
@@ -47,7 +46,7 @@ function Combo(params) {
 		oCombo.isExpanded = false;
 		this.detach();
 	};
-
+	
 	var cont = new Widget({
 		width : '100%',
 		height: '100%',
@@ -72,7 +71,7 @@ function Combo(params) {
 	resizer.attachEvent('onmousedown', function(evt){
 		oCombo.dropdown._startResize(evt);
 	});
-
+	
 	this.button = new XButton({
 		left : 'this.parent.getWidth()-20',
 		height : '100%', width : 20,
@@ -85,9 +84,14 @@ function Combo(params) {
 	if (this.editable) {
 		e.value = (params.value)?params.value:'';
 		if (!this.readonly) {
-			e.onchange = function() {
-				if (oCombo._customRegistry.onchange)
-					QuiX.getEventListener(oCombo._customRegistry.onchange)(oCombo);
+			e.onfocus = function() {
+				oCombo._old_value = this.value;
+			}
+			e.onblur = function() {
+				if (oCombo._old_value != this.value) {
+					if (oCombo._customRegistry.onchange)
+						QuiX.getEventListener(oCombo._customRegistry.onchange)(oCombo);
+				}
 			}
 		}
 		else
@@ -134,8 +138,14 @@ Combo.prototype.getValue = function() {
 }
 
 Combo.prototype.setValue = function(value) {
-	if (this.editable)
+	if (this.editable){
 		this.div.firstChild.value = value;
+		if (this._old_value != value) {
+			if (this._customRegistry.onchange)
+				QuiX.getEventListener(this._customRegistry.onchange)(this);
+		}		
+		this._old_value = value;
+	}
 	else {
 		var opt, opt_value;
 		var old_value = this.getValue();
@@ -201,25 +211,24 @@ Combo.prototype.clearOptions = function() {
 }
 
 Combo.prototype.showDropdown = function(w) {
-	var oCombo = w || this;
+	var w = w || this;
+	var iLeft = w.getScreenLeft();
+	var iTop = w.getScreenTop() + w.getHeight(true);
 
-	var iLeft = oCombo.getScreenLeft();
-	var iTop = oCombo.getScreenTop() + oCombo.getHeight(true);
+	if (iTop + w.menuHeight > document.desktop.getHeight(true))
+		iTop = w.getScreenTop() - w.menuHeight;
 
-	if (iTop + oCombo.menuHeight > document.desktop.getHeight(true))
-		iTop = oCombo.getScreenTop() - oCombo.menuHeight;
+	w.dropdown.top = iTop;
+	w.dropdown.left = iLeft;
+	if (!w.dropdown.width)
+		w.dropdown.width = 'this.combo.getWidth(true)';
+	w.dropdown.height = w.menuHeight;
+	w.dropdown.setBgColor(w.getBgColor());
 
-	oCombo.dropdown.top = iTop;
-	oCombo.dropdown.left = iLeft;
-	if (!oCombo.dropdown.width)
-		oCombo.dropdown.width = 'this.combo.getWidth(true)';
-	oCombo.dropdown.height = oCombo.menuHeight;
-	oCombo.dropdown.setBgColor(oCombo.getBgColor());
-
-	document.desktop.appendChild(oCombo.dropdown);
-	oCombo.dropdown.redraw();
-	document.desktop.overlays.push(oCombo.dropdown);
-	oCombo.isExpanded = true;
+	document.desktop.appendChild(w.dropdown);
+	w.dropdown.redraw();
+	document.desktop.overlays.push(w.dropdown);
+	w.isExpanded = true;
 }
 
 Combo.prototype.destroy = function() {
@@ -233,16 +242,17 @@ Combo.prototype.setBgColor = function(color) {
 		this.div.firstChild.style.backgroundColor = color;
 }
 
-Combo.prototype.addOption = function(params) {
+Combo.prototype.addOption = function(params, w) {
+	var w = w || this;
 	params.align = params.align || 'left';
 	params.width = params.width || '100%';
 	var opt = new Icon(params);
 	opt._isContainer = false;
 	opt.selected = false;
 	opt.value = params.value;
-	this.dropdown.widgets[0].appendChild(opt);
-	if ((params.selected=='true' || params.selected == true) && !this.editable) {
-		this.selectOption(opt);
+	w.dropdown.widgets[0].appendChild(opt);
+	if ((params.selected=='true' || params.selected == true) && !w.editable) {
+		w.selectOption(opt);
 	}
 	opt.attachEvent('onmouseover', ComboOption__mouseover);
 	opt.attachEvent('onmouseout', ComboOption__mouseout);
@@ -280,6 +290,124 @@ function ComboBtn__onclick(evt, w) {
 	QuiX.stopPropag(evt);
 }
 
+// auto complete
+function AutoComplete(params) {
+	params = params || {};
+	params.editable = true;
+	this.base = Combo;
+	this.base(params);
+	this.textField = this.div.firstChild;
+	this.url = params.url;
+	this.method = params.method;
+	if (this.url == '/')
+		this.url = '';
+	
+	//hide combo button
+	this.widgets[0].hide();
+	
+	//attach events
+	var oAuto = this;
+	this.textField.onkeyup = function(evt) {
+		var evt = evt || event;
+		oAuto._captureKey(evt);
+	}
+}
+
+QuiX.constructors['autocomplete'] = AutoComplete;
+AutoComplete.prototype = new Combo;
+
+AutoComplete.prototype._getSelection = function(evt) {
+	var sel = this.dropdown.widgets[0].getWidgetsByClassName('option over');
+	var index = -1;
+	if (sel.length > 0) {
+		sel = sel[0];
+		index = this.options.indexOf(sel);
+		sel.div.className = 'option';
+	}
+	
+	switch(evt.keyCode) {
+		case 40: 
+			if (index + 1 < this.options.length)
+				return index + 1;
+			return 0;
+		case 38: 
+			return (index == 0)?this.options.length-1:index-1;
+		case 13:
+			return index;
+	}
+}
+
+AutoComplete.prototype._getResults = function() {
+	var xmlrpc = new XMLRPCRequest(QuiX.root + this.url);
+	xmlrpc.oncomplete = this._showResults;
+	xmlrpc.callback_info = this;
+	xmlrpc.callmethod(this.method, this.textField.value);
+}
+
+AutoComplete.prototype._showResults = function(oReq) {
+	var oAuto = oReq.callback_info;
+	oAuto.dropdown.widgets[0].clear();
+	if (oReq.response.length > 0) {
+		for (var i=0; i<oReq.response.length; i++)
+			oAuto.addOption(oReq.response[i]);
+		if (!oAuto.isExpanded)
+			oAuto.showDropdown();
+		oAuto.dropdown.redraw();
+	}
+	else {
+		if (oAuto.isExpanded)
+			oAuto.dropdown.close();
+	}
+}
+
+AutoComplete.prototype._captureKey = function(evt) {
+	var index;
+	if (this.textField.value == '') {
+		if (this.isExpanded)
+			this.dropdown.close();
+		this.dropdown.widgets[0].clear();
+		return;
+	}
+
+	switch(evt.keyCode)
+	{
+		case 27: //ESC
+		case 39: //Right Arrow
+			if (this.isExpanded)
+				this.dropdown.close();
+			break;
+		case 40: // Down Arrow
+			if (this.options.length == 0) return;
+			if (!this.isExpanded)
+				this._getResults();
+			else {
+				index = this._getSelection(evt);
+				var opt = this.options[index];
+				this.dropdown.widgets[0].div.scrollTop = opt.div.offsetTop - 20;
+				opt.div.className = 'option over';
+			}
+			break;
+		case 38: //Up arrow
+			index = this._getSelection(evt);
+			if (index < 0)
+				return;
+			if (!this.isExpanded)
+				this.showDropdown();
+			var opt = this.options[index];
+			this.dropdown.widgets[0].div.scrollTop = opt.div.offsetTop - 20;
+			opt.div.className = 'option over';
+			break;
+		case 13: // enter
+			index = this._getSelection(evt);
+			if (!this.isExpanded || index < 0)
+				return;
+			this.dropdown.close();
+			this.selectOption(this.options[index]);
+			break;
+		default:
+			this._getResults();
+	}
+}
 
 // Select list
 function SelectList(params) {
