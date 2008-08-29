5cd4d2b16aaa035279deba79d45bb5f3477b3342
tkouts <tkouts@4958b8bd-3100-0410-97a7-c2b44273271c>
2008-08-29 15:48:36 +0000

    QuiX DataGrid:   - real-time updating of the dataset   - navigation between cells using the TAB key

--- porcupine/trunk/quix/datagrid.js
+++ porcupine/trunk/quix/datagrid.js
@@ -22,6 +22,7 @@ DataGrid.prototype = new ListView;
 DataGrid.prototype.addHeader = function(params) {
 	var oHeader = ListView.prototype.addHeader(params, this);
 	this.widgets[1].attachEvent('onclick', DataGrid__onclick);
+	this.widgets[1].attachEvent('onkeydown', DataGrid__onkeydown);
 	return(oHeader);
 }
 
@@ -54,71 +55,117 @@ DataGrid.prototype.refresh = function() {
 	ListView.prototype.refresh(this);
 }
 
-function DataGrid__onclick(evt, w) {
-	var target=null, idx, ridx, editValue, w2, w2_type;
-	target = QuiX.getTarget(evt);
-	while (target.tagName!='TD' && target!=document.body)
-		target = target.parentElement || target.parentNode;
-	if (target!=document.body) {
-		idx = target.cellIndex;
-		ridx = (target.parentElement || target.parentNode).rowIndex;
-		if (idx>0 && idx<w.parent.columns.length-1 && w.parent.columns[idx].editable) {
-			editValue = w.parent.dataSet[ridx][w.parent.columns[idx].name];
-			if (typeof editValue == 'undefined' && !w.parent.editUndef)
-				return;
-			switch (w.parent.columns[idx].columnType) {
-				case 'optionlist':
-					w2 = new Combo({
-						top: target.offsetTop,
-						left: target.offsetLeft,
-						width: target.offsetWidth,
-						height: target.offsetHeight
-					});
-					w.appendChild(w2);
-				
-					var options = w.parent.columns[idx].options;
-					for (var i=0; i<options.length; i++) {
-						if (editValue==options[i].value)
-							options[i].selected = true;
-						else
-							options[i].selected = false;
-						w2.addOption(options[i]);
-					}
-					break;
-				case 'bool':
-					w2_type = 'checkbox';
-				default:
-					w2 = new Field({
-						top: target.offsetTop,
-						left: target.offsetLeft,
-						width: target.offsetWidth,
-						height: target.offsetHeight,
-						value: editValue,
-						type: w2_type
-					});
-					w.appendChild(w2);
-			}
-			w2.redraw();
-			w.parent.attributes.__editwidget = w2;
-			w.parent.attributes.__rowindex = ridx;
-			w.parent.attributes.__cellindex = idx;
-			w.parent.attachEvent('onmousedown', DataGrid__onmousedown);
+DataGrid.prototype.edit = function(cell) {
+	var editValue, w2, w2_type;
+	var idx = cell.cellIndex;
+	var ridx = (cell.parentElement || cell.parentNode).rowIndex;
+	if (idx>0 && idx<this.columns.length-1 && this.columns[idx].editable) {
+		editValue = this.dataSet[ridx][this.columns[idx].name];
+		if (typeof editValue == 'undefined' && !this.editUndef)
+			return;
+		switch (this.columns[idx].columnType) {
+			case 'optionlist':
+				w2 = new Combo({
+					top : cell.offsetTop,
+					left : cell.offsetLeft,
+					width : cell.offsetWidth,
+					height : cell.offsetHeight,
+					onchange : DataGrid__update
+				});
+				this.widgets[1].appendChild(w2);
+			
+				var options = this.columns[idx].options;
+				for (var i=0; i<options.length; i++) {
+					if (editValue==options[i].value)
+						options[i].selected = true;
+					else
+						options[i].selected = false;
+					w2.addOption(options[i]);
+				}
+				break;
+			case 'bool':
+				w2_type = 'checkbox';
+			default:
+				w2 = new Field({
+					top : cell.offsetTop,
+					left : cell.offsetLeft,
+					width : cell.offsetWidth,
+					height : cell.offsetHeight,
+					value : editValue,
+					type : w2_type
+				});
+				if (w2_type == 'checkbox')
+					w2.attachEvent('onchange', DataGrid__update)
+				else
+					w2.attachEvent('onkeyup', DataGrid__update)
+				this.widgets[1].appendChild(w2);
 		}
+		w2.redraw();
+		if (w2.focus) w2.focus();
+		this.attributes.__editwidget = w2;
+		this.attributes.__rowindex = ridx;
+		this.attributes.__cellindex = idx;
+		this.attachEvent('onmousedown', DataGrid__onmousedown);
 	}
 }
 
-function DataGrid__onmousedown (evt, w) {
-	var rowindex = w.attributes.__rowindex;
-	var cellindex = w.attributes.__cellindex;
-	var oCell = w.list.rows[rowindex].cells[cellindex];
-	var value = w.attributes.__editwidget.getValue();
-	w.dataSet[rowindex][w.columns[cellindex].name] = value;
-	w._renderCell(oCell, cellindex, value);
+function DataGrid__onclick(evt, w) {
+	var target = QuiX.getTarget(evt);
+	while (target && target.tagName != 'TD')
+		target = target.parentElement || target.parentNode;
+	if (target)
+		w.parent.edit(target);
+}
 
+function DataGrid__onkeydown(evt, w) {
+	if (evt.keyCode == 9) {
+		var dg = w.parent; 
+		var r = dg.attributes.__rowindex;
+		var c = dg.attributes.__cellindex;
+		var rows = dg.list.rows;
+		var current_cell = rows[r].cells[c]; 
+		if (evt.shiftKey) {
+			do {
+				current_cell = current_cell.previousSibling;
+				if (!current_cell) {
+					if (r > 0)
+						current_cell = rows[r-1].cells[dg.columns.length-2];
+					else
+						current_cell = rows[rows.length-1].cells[dg.columns.length-2];
+				}				
+			} while (!dg.columns[current_cell.cellIndex].editable)
+		}
+		else {
+			do {
+				current_cell = current_cell.nextSibling;
+				if (!current_cell) {
+					if (r < rows.length - 1)
+						current_cell = rows[r+1].cells[0];
+					else
+						current_cell = rows[0].cells[0];
+				}
+			} while (!dg.columns[current_cell.cellIndex].editable)
+		}
+		dg._removeEditWidget();
+		dg.edit(current_cell);
+		QuiX.cancelDefault(evt);
+	}
+}
+
+function DataGrid__update(evt, w) {
+	var w = w || evt;
+	var dg = w.parent.parent;
+	var r = dg.attributes.__rowindex;
+	var c = dg.attributes.__cellindex;
+	var cell = dg.list.firstChild.rows[r].cells[c];
+	var value = dg.attributes.__editwidget.getValue();
+	window.status = value;
+	dg.dataSet[r][dg.columns[c].name] = value;
+	dg._renderCell(cell, c, value);
+}
+
+function DataGrid__onmousedown(evt, w) {
 	w.attributes.__editwidget.destroy();
 	w.attributes.__editwidget = null;
-
 	w.detachEvent('onmousedown');
-	
-	delete w.attributes.__editwidget;
 }
